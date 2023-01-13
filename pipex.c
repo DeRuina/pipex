a910d305c271a86a2da0c1c2e449df06dc36d5c4
Dean Ruina <druina@c2r5p11.hive.fi>
2023-01-13 17:10:58 +0200

    basic structure ready

--- pipex.c
+++ pipex.c
@@ -6,20 +6,20 @@
 /*   By: druina <druina@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 14:04:34 by druina            #+#    #+#             */
-/*   Updated: 2023/01/13 15:59:01 by druina           ###   ########.fr       */
+/*   Updated: 2023/01/13 17:06:22 by druina           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-#define PROCESS_NUMBER argc - 4
+#define PROCESS_NUMBER argc - 3
 
 int	childzero(char **argv, int **pipe_n)
 {
-	int fd;
-	int *str;
+	int	fd;
+	int	*str;
+
 	if ((fd = open(argv[1], O_RDONLY)) == -1)
 		error("woah, file opening problem");
-
 	return (EXIT_SUCCESS);
 }
 
@@ -31,14 +31,14 @@ void	error(char *msg)
 
 int	main(int argc, char **argv /*, char **env*/)
 {
-	int	pipe_n[PROCESS_NUMBER][2];
+	int	pipe_n[PROCESS_NUMBER - 1][2];
 	int	i;
 	int	pid[PROCESS_NUMBER];
 	int	j;
 
 	j = 0;
 	i = 0;
-	while (i < PROCESS_NUMBER)
+	while (i < PROCESS_NUMBER - 1)
 	{
 		if (pipe(pipe_n[i]) == -1)
 			error("woah, pipe problem");
@@ -52,7 +52,7 @@ int	main(int argc, char **argv /*, char **env*/)
 			error("woah, fork problem");
 		if (pid[i] == 0)
 		{
-			while (j < PROCESS_NUMBER)
+			while (j < PROCESS_NUMBER - 1)
 			{
 				if (i != j)
 				{
@@ -62,16 +62,15 @@ int	main(int argc, char **argv /*, char **env*/)
 				j++;
 			}
 			if (pid[0])
-				return(childzero(argv, pipe_n));
+				return (childzero(argv, pipe_n));
 			return (EXIT_SUCCESS);
 		}
 		i++;
 	}
 	j = 0;
-	while (j < PROCESS_NUMBER)
+	while (j < PROCESS_NUMBER - 1)
 	{
-		if (j != PROCESS_NUMBER - 1)
-			close(pipe_n[j][0]);
+		close(pipe_n[j][0]);
 		close(pipe_n[j][1]);
 		j++;
 	}
